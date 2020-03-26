4cadb810f20054ce441e447e98a3fcbbfbb7464f
Garrett <gmaitlan@emich.edu>
2020-03-26 15:21:26 -0400

    Camera scrolling has been improved.

--- Warlust/Assets/__Scripts/CameraController.cs
+++ Warlust/Assets/__Scripts/CameraController.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 	[Header("Set in Inspecrot")]
-	public float scrollSpeed = 1f;
+	public float scrollSpeed = 0.05f;
+	private enum scrollingDirection {
+		neutral, up, down, left, right
+	}
+	private scrollingDirection xDirection;
+	private scrollingDirection yDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        xDirection = scrollingDirection.neutral;
+		yDirection = scrollingDirection.neutral;
     }
 
     // Update is called once per frame
     void Update() {
-		if (Input.GetKeyDown("up")) {
+		if (Input.GetKeyUp("up") || Input.GetKeyUp("down")) {
+			yDirection = scrollingDirection.neutral;
+		} else if (yDirection == scrollingDirection.up || Input.GetKeyDown("up")) {
 			Vector3 temp = transform.position;
 			temp.y += scrollSpeed;
 			transform.position = temp;
-		} else if (Input.GetKeyDown("down")) {
+			yDirection = scrollingDirection.up;
+		} else if (yDirection == scrollingDirection.down || Input.GetKeyDown("down")) {
 			Vector3 temp = transform.position;
 			temp.y -= scrollSpeed;
 			transform.position = temp;
+			yDirection = scrollingDirection.down;
 		}
-		if (Input.GetKeyDown("left")) {
+		if (Input.GetKeyUp("left") || Input.GetKeyUp("right")) {
+			xDirection = scrollingDirection.neutral;
+		} else if (xDirection == scrollingDirection.left || Input.GetKeyDown("left")) {
 			Vector3 temp = transform.position;
 			temp.x -= scrollSpeed;
 			transform.position = temp;
-		} else if (Input.GetKeyDown("right")) {
+			xDirection = scrollingDirection.left;
+		} else if (xDirection == scrollingDirection.right || Input.GetKeyDown("right")) {
 			Vector3 temp = transform.position;
 			temp.x += scrollSpeed;
 			transform.position = temp;
+			xDirection = scrollingDirection.right;
 		}
     }
 }
