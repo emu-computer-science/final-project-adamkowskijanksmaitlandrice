72cd7b630d759967cfef74c4433b0a682ab0118f
Michael Janks <mjanks@emich.edu>
2020-04-11 16:37:11 -0400

    Options menu working, still testing resolution setting

---
+++ Warlust/Assets/Control.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+ 
+public class Control : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
---
+++ Warlust/Assets/GoToOptions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+ 
+public class GoToOptions : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Options");
+    }
+}
