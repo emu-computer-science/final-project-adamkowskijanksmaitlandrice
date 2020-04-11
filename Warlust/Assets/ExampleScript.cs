1590dfa5e27bc9ca422a5c289be360a3e1eb0f9b
Michael Janks <mjanks@emich.edu>
2020-04-11 15:30:04 -0400

    Added fonts, fixed scaling on tactical screen

---
+++ Warlust/Assets/ExampleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+	public void Hello() {
+		Debug.Log("Hi I'm Mike");
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
