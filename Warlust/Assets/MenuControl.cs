4796366a354e8e80ce0e536ca4f6f695e4abdef9
Michael Janks <mjanks@emich.edu>
2020-04-11 23:13:27 -0400

    Title/option changes

---
+++ Warlust/Assets/MenuControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+	public void ButtonOptions() {
+		SceneManager.LoadScene(4);
+	}
+
+	public void ButtonQuit() {
+		Application.Quit();
+	}
+   
+}
